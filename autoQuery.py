52198a8afd932e8d6fbcfee9f8c5ed504f0ea8b5
sergey-khl <64624048+sergey-khl@users.noreply.github.com>
2022-07-22 20:28:01 -0600

    Update autoQuery.py

--- autoQuery.py
+++ autoQuery.py
@@ -5,19 +5,10 @@ import csv
 
 stamp = datetime.today().strftime("%Y%m%d")
 
-file_install = "location\of\install\data{}.csv".format(stamp) # set this to where you want your download to be made. The csv need not exist yet.
+file_install = "location\of\install.csv\data{}.csv".format(stamp) # set this to where you want your download to be made. The csv need not exist yet.
 showConsole = True # make this false if you do not want to see the terminal pop up with the values.
 
 if __name__ == '__main__':
-    info = (int(sys.argv[1]), int(sys.argv[2]))
-    s_date = datetime.today() - timedelta(days=info[0])
-    e_date = datetime.today() - timedelta(days=info[0]) + timedelta(days=info[1])
-
-    if showConsole:
-        print('start date: ' + str(s_date) + 'end_date: ' + str(e_date))
-
-    
-
     conn = mysql.connector.connect(user='user', password='pass',
                              host='127.0.0.1',
                              database='')
